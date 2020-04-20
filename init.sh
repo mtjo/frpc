a5b05576f60797ea4ce405274aad5016021c408e
韦洪将 <weihongjiang@ecpark.cn>
2020-04-21 01:38:25 +0800

    add getconfig

--- init.sh
+++ init.sh
@@ -1,8 +1,22 @@
 #!/bin/ash
 model=$(cat /proc/xiaoqiang/model)
+setconfig model $model
+version=$(/frp/frpc -v);
+setconfig version $version
+configType=$(getconfig configType)
+if [[ "$configType" == "base" ]]; then
+	/frp/frpc -c /etc/frpc_config.ini &>/dev/null 
+else
+	/frp/frpc -c /etc/frpc_user_config.ini &>/dev/null
+fi
+pid=$(pidof frpc);
 
 while :
 do
-date >> aa.log
-sleep 3;
+	PID=$(pidof frpc);
+	if [[ "$pid" != "$PID" ]]; then
+		setconfig PID $PID
+		pid=$PID
+	fi
+	sleep 3
 done
